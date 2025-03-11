565858dd3f198ad209c28ebae0865cdeb83fdf06
yan <15316488+fish-yan@users.noreply.github.com>
2025-03-11 14:36:49 +0800

    init

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Web3Extension"),
+        .target(name: "Web3Extension"),
         .testTarget(
             name: "Web3ExtensionTests",
             dependencies: ["Web3Extension"]
